fe591ebad3123480e8ee6d330f7efe682028d2f7
Tommaso Acciarresi <49035039+tom-acciarresi@users.noreply.github.com>
2026-05-28 18:40:27 +0200

    ugh

--- app/src/main/java/it/unicam/cs/mpgc/rpg130730/MyApplication.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg130730/MyApplication.java
@@ -1,14 +1,21 @@
 package it.unicam.cs.mpgc.rpg130730;
 
 import javafx.application.Application;
+// import javafx.fxml.FXMLLoader;
+// import javafx.scene.Parent;
+// import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MyApplication extends Application {
     private final String APPLICATION_TITLE = "TestProject";
 
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle(APPLICATION_TITLE);
-        primaryStage.show();
+    public void start(Stage stage) throws Exception {
+        // Parent root = FXMLLoader.load(getClass().getResource("fxml_example.fxml"));
+        // Scene scene = new Scene(root, 300, 275);
+
+        stage.setTitle(APPLICATION_TITLE);
+        // stage.setScene(scene);
+        stage.show();
     }
 }
